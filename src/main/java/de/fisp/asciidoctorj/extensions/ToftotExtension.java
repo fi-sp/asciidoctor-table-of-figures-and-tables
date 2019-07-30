ac3f1b5272a0ab813e15f0bd75d83cd106d3c4c8
Christian Wahnsiedler <Christian.Wahnsiedler@f-i-sp.de>
2019-07-30 09:09:06 +0200

    initial commit

---
+++ src/main/java/de/fisp/asciidoctorj/extensions/ToftotExtension.java
@@ -0,0 +1,13 @@
+package de.fisp.asciidoctorj.extensions;
+
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.extension.JavaExtensionRegistry;
+import org.asciidoctor.jruby.extension.spi.ExtensionRegistry;
+
+public class ToftotExtension implements ExtensionRegistry { // (1)
+    @Override
+    public void register(Asciidoctor asciidoctor) { // (2)
+        JavaExtensionRegistry javaExtensionRegistry = asciidoctor.javaExtensionRegistry();// (3)
+        javaExtensionRegistry.preprocessor(ToftotPreProcessor.class);
+    }
+}
